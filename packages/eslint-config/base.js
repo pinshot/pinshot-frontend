e53ea766359d827da0f871551885b8f8cf1943f7
jada-gwon <jada.gwon@gmail.com>
2024-12-20 10:56:15 +0900

    chore: eslint import order 규칙 설정

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -4,6 +4,7 @@ import turboPlugin from 'eslint-plugin-turbo';
 import tseslint from 'typescript-eslint';
 import onlyWarn from 'eslint-plugin-only-warn';
 import tailwind from 'eslint-plugin-tailwindcss';
+import importPlugin from 'eslint-plugin-import';
 
 /**
  * A shared ESLint configuration for the repository.
@@ -12,6 +13,8 @@ import tailwind from 'eslint-plugin-tailwindcss';
  * */
 export const config = [
   js.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
   ...tailwind.configs['flat/recommended'],
@@ -19,8 +22,64 @@ export const config = [
     plugins: {
       turbo: turboPlugin,
     },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+
     rules: {
       'turbo/no-undeclared-env-vars': 'warn',
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          distinctGroup: false,
+          groups: [
+            ['builtin', 'external'],
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'unknown',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'after',
+            },
+            {
+              pattern: 'react/**',
+              group: 'builtin',
+              position: 'after',
+            },
+            {
+              pattern: 'next',
+              group: 'builtin',
+              position: 'after',
+            },
+            {
+              pattern: 'next/**',
+              group: 'builtin',
+              position: 'after',
+            },
+            {
+              pattern: '@/shared/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react', 'react/**'],
+        },
+      ],
     },
   },
   {
